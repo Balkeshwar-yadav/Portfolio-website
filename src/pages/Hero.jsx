9731e32ea4bf6581334a37830b8ae844b8c737af
ac4370879@gmail.com <ac4370879@gmail.com.>
2026-04-03 10:47:21 +0530

    Adding Projects

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function Hero() {
   return (
     <section className="min-h-screen bg-gradient-to-br from-gray-900 via-black to-gray-800 text-white flex items-center justify-center px-6">
@@ -20,12 +22,8 @@ export default function Hero() {
 
           {/* BUTTONS */}
           <div className="flex gap-4 mt-8">
-            <a
-              href="/projects"
-              className="bg-blue-600 hover:bg-blue-700 px-6 py-3 rounded-lg font-medium transition"
-            >
-              View Projects
-            </a>
+           
+            <Link to='/projects' className="bg-blue-600 hover:bg-blue-700 px-6 py-3 rounded-lg font-medium transition">View Projects</Link>
 
             <a
               href="/images/Resume.pdf" // path to your PDF
